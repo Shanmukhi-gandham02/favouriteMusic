c262f5c04dd6e60cee47d2f985dc63d8127f9ef9
Shanmukhi-gandham02 <shanmukhigandham16@gmail.com>
2024-01-15 16:43:34 +0530

    commit

--- favouriteMusic/settings.py
+++ favouriteMusic/settings.py
@@ -128,8 +128,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = os.path.join(BASE_DIR, 'music_tracker/static'),
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'music_tracker/static')
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
